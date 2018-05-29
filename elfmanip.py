82e67ad0444455b93b45e984b6023160cc3e68f8
Alexandre Joannou <aj443@cam.ac.uk>
2018-05-29 13:19:33 +0100

    Removed use of a single bytearray for the whole elf

--- elfmanip.py
+++ elfmanip.py
@@ -22,6 +22,8 @@ parser.add_argument('--only-section', nargs='+', default=None, metavar='SECTION'
                     help="List the elf SECTIONs to use when generating the output (default all).")
 parser.add_argument('--exclude-section', nargs='+', default=None, metavar='SECTION',
         help="List the elf SECTIONs to exclude when generating the output (default none). Note: SECTIONs listed here will be excluded from the sections specified with --only-section as well.")
+parser.add_argument('-f', '--force-skip', action="store_true",
+                    help="In case a section overlaps with an earlier section, force skipping.")
 
 subcmds = parser.add_subparsers(dest='subcmd',metavar='sub-command',help="Individual sub-command help available by invoking it with -h or --help.")
 subcmds.required = True
@@ -29,16 +31,12 @@ subcmds.required = True
 # hex
 hexcmd = subcmds.add_parser('hex',
                     help="Generate a HEX file.")
-hexcmd.add_argument('-f', '--force-skip', action="store_true",
-                    help="In case a section overlaps with an earlier section, force skipping.")
 hexcmd.add_argument('-w', '--word-size', type=auto_int, default=4, metavar="BYTE_WIDTH",
                     help="The size in bytes of a memory word.")
 
 # mif
 mifcmd = subcmds.add_parser('mif',
                     help="Generate a MIF file.")
-mifcmd.add_argument('-f', '--force-skip', action="store_true",
-                    help="In case a section overlaps with an earlier section, force skipping.")
 mifcmd.add_argument('-w', '--word-size', type=auto_int, default=4, metavar="BYTE_WIDTH",
                     help="The size in bytes of a memory word.")
 #binary (BIN), hexadecimal (HEX), octal (OCT), signed decimal (DEC), unsigned decimal (UNS)
@@ -70,7 +68,7 @@ def field_width(width, rad):
   tmp = int.from_bytes(bytearray(b'\xff') * width, 'little')
   return len("{:{rd}}".format(tmp, rd=rad_to_fmt(rad)))
 
-def group_bytes(some_bytes, group_size):
+def group_bytes_padded(some_bytes, group_size):
   for i in range(0, len(some_bytes), group_size):
     tmp = some_bytes[i:i+group_size]
     if len(tmp) == group_size:
@@ -80,76 +78,111 @@ def group_bytes(some_bytes, group_size):
       val[0:len(tmp)] = tmp
       yield val
 
-def dump_hex(some_bytes, some_width):
+def group_bytes(some_bytes, group_size):
+  for i in range(0, len(some_bytes), group_size):
+    yield some_bytes[i:i+group_size]
+
+def dump_hex(some_bytes):
   dump = ""
-  word = ""
+  for b in some_bytes:
+    dump = "{:02X}{:s}".format(b,dump)
+  return dump
+
+def dump_hex_padded(some_bytes, some_width):
+  dump = ""
   i = 0
   for b in some_bytes:
-    word = "{:02X}{:s}".format(b,word)
+    dump = "{:02X}{:s}".format(b,dump)
     i += 1
-    if i == some_width:
-      dump += "{:s}\n".format(word)
-      word = ""
-      i = 0
-  if i != 0:
-    dump += "{:s}{:s}\n".format("00"*(some_width-i), word)
+  if i < some_width:
+      dump = "{:s}{:s}".format("00"*(some_width-i),dump)
   return dump
 
 def input_y_n(prompt):
   s = input(prompt)
   return s.lower() in ["", "y", "ye", "yes"]
 
-#########################
-# sections to bytearray #
-#########################
+##########################
+# elf sections filtering #
+##########################
 
-def elf_sections_to_bytearray(sections, force_skip=False):
-  last_section = sorted(sections, key = lambda x: x.header["sh_addr"])[-1]
-  size = last_section.header["sh_addr"] + last_section.header["sh_size"]
-  data_bytes = bytearray(size)
-  #print(size)
+def filter_elf_sections(sections, only=None, exclude=None, force_skip=False): 
+  filtered = list(sections)
+  if only:
+    filtered = [x for x in filtered if str(x.name) in only]
+  if exclude:
+    filtered = [x for x in filtered if str(x.name) not in exclude]
   last_top = 0
-  for section in sections:
+  for s in filtered:
     skip = False
-    addr = section.header["sh_addr"]
+    addr = s.header["sh_addr"]
+    top = addr + s.header["sh_size"] # XXX check data sz compared to section sz
     if addr < last_top:
       if force_skip:
         skip = True
       else:
-        skip = input_y_n("{:s} overlaps with previous sections. Skip {:s} (Y/n) ? ".format(*[section.name]*2))
+        skip = input_y_n("{:s} overlaps with previous sections. Skip {:s} (Y/n) ? ".format(*[str(s.name)]*2))
     if not skip:
-      last_top = addr + section.header["sh_size"] # XXX check data sz compared to section sz
-      data_bytes[addr : last_top] = section.data()
-      print("{:s} section written (0x{:0X} to 0x{:0X})".format(section.name, addr, last_top))
-  return data_bytes
+      last_top = top
+    else:
+      filtered = [x for x in filtered if str(x.name) != str(s.name)]
+  return filtered
 
 ##################
 # write hex file #
 ##################
 
 def elf_sections_to_hex(sections, outfile, byte_width, force_skip=False):
-  outfile.write(dump_hex(elf_sections_to_bytearray(sections, force_skip), byte_width))
+  ssections = sorted(sections, key = lambda x: x.header["sh_addr"])
+  last_addr = 0
+  remaining = bytearray(0)
+  for s in ssections:
+    # skip empty sections
+    if s.header["sh_size"] == 0:
+      continue
+    # sections with content...
+    print("starting sections {:s} with last_addr 0x{:x} remaining {:s}".format(str(s.name), last_addr, str(remaining)))
+    addr  = s.header["sh_addr"]
+    padding = remaining # fold in unaligned bytes from previous section
+    if addr > last_addr: # fill gap with zeroes
+      padding += bytearray(addr - last_addr)
+    remaining = bytearray(0) # reset remaining bytes
+    for g in group_bytes(padding + s.data(), byte_width):
+      if len(g) == byte_width:
+        outfile.write("{:s}\n".format(dump_hex(g)))
+      else:
+        remaining = g
+    # prepare next step
+    last_addr = addr + s.header["sh_size"]
+  # remaining bytes to write ...
+  if len(remaining) != 0:
+    outfile.write(dump_hex_padded(padding, byte_width))
 
 ##################
 # write mif file #
 ##################
 
-def elf_sections_to_mif(sections, outfile, byte_width, addr_rad, data_rad, force_skip=False):
-  data_bytes = elf_sections_to_bytearray(sections, force_skip)
-  nb_words = int(len(data_bytes)/byte_width)
+def elf_sections_to_mif(sections, outfile, byte_width, addr_rad, data_rad):
+  ssections = sorted(sections, key = lambda x: x.header["sh_addr"])
+  last_addr = ssections[-1].header["sh_addr"] + ssections[-1].header["sh_size"]
+  nb_words = int(last_addr/byte_width)
   outfile.write("DEPTH = {:d};\n".format(nb_words))
   outfile.write("WIDTH = {:d};\n".format(byte_width*8))
   outfile.write("ADDRESS_RADIX = {:s};\n".format(addr_rad))
   outfile.write("DATA_RADIX = {:s};\n".format(data_rad))
   outfile.write("CONTENT\n")
   outfile.write("BEGIN\n")
-  addr = 0
-  max_aw = len("{:{rad}}".format(len(data_bytes),rad=rad_to_fmt(addr_rad)))
+  max_aw = len("{:{rad}}".format(nb_words,rad=rad_to_fmt(addr_rad)))
   max_dw = field_width(byte_width, data_rad)
-  for word in group_bytes(data_bytes, byte_width):
-    data = int.from_bytes(word, byteorder='little', signed=(True if data_rad == 'DEC' else False))
-    outfile.write("{:0{aw}{rad_a}}: {:0{dw}{rad_d}};\n".format(addr, data, aw=max_aw, rad_a=rad_to_fmt(addr_rad), dw=max_dw, rad_d=rad_to_fmt(data_rad)))
-    addr += 1
+  addr = 0
+  for s in ssections:
+    if s.header["sh_addr"] > addr: # fill gap with zeroes
+      outfile.write("[{:0{aw}{rad_a}}..{:0{aw}{rad_a}}]: {:0{dw}{rad_d}};\n".format(addr, s.header["sh_addr"]-1, 0, aw=max_aw, rad_a=rad_to_fmt(addr_rad), dw=max_dw, rad_d=rad_to_fmt(data_rad)))
+      addr = s.header["sh_addr"]
+    for word in group_bytes_padded(s.data(), byte_width):
+      data = int.from_bytes(word, byteorder='little', signed=(True if data_rad == 'DEC' else False))
+      outfile.write("{:0{aw}{rad_a}}: {:0{dw}{rad_d}};\n".format(addr, data, aw=max_aw, rad_a=rad_to_fmt(addr_rad), dw=max_dw, rad_d=rad_to_fmt(data_rad)))
+      addr += 1
   outfile.write("END;")
 
 #################
@@ -163,17 +196,12 @@ def main():
   print("args.output: {:s}".format(args.output))
   with open(args.elf,"rb") as in_f:
     elf = ELFFile(in_f)
-    if args.only_section:
-      sections = [x for x in elf.iter_sections() if x.name in args.only_section]
-    else:
-      sections = list(elf.iter_sections())
-    if args.exclude_section:
-      sections = [s for s in sections if s.name not in args.exclude_section]
+    sections = filter_elf_sections(elf.iter_sections(), args.only_section, args.exclude_section, args.force_skip)
     with open(args.output,"w") as out_f:
       if (args.subcmd == "hex"):
-        elf_sections_to_hex(sections, out_f, args.word_size, force_skip=args.force_skip)
+        elf_sections_to_hex(sections, out_f, args.word_size)
       if (args.subcmd == "mif"):
-        elf_sections_to_mif(sections, out_f, args.word_size, args.address_radix, args.data_radix, force_skip=args.force_skip)
+        elf_sections_to_mif(sections, out_f, args.word_size, args.address_radix, args.data_radix)
     exit(0)
 
 if __name__ == "__main__":
